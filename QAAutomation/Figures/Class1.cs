fd1d823dfac1f8b21ce578c853dcf298dda41c5b
RomanGlobal <dlyaspam2@gmail.com>
2017-06-08 19:32:02 +0300

    1st commit

---
+++ QAAutomation/Figures/Class1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figures
+{
+    public abstract class Figure
+    {
+        public event EventHandler<string> PerimeterCalculated;
+
+        public int Length { get; set; }
+
+        private int Radius { get; set; }
+
+        public Figure(int Length)
+        {
+            this.Radius = 1;
+            this.Length = Length;
+            Console.WriteLine("base figure created");
+        }
+
+        public virtual int Perimeter()
+        {
+            Console.WriteLine("empty base perimeter");
+            if (PerimeterCalculated != null)
+                PerimeterCalculated(this, "Base class generated perimer event");
+
+            return 0;
+        }
+
+        public int Square()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+
+    public class Square : Figure
+    {
+        public Square(int Length) : base(Length)
+        {
+
+
+            Console.WriteLine("square created");
+        }
+
+        public override int Perimeter()
+        {
+            var res = base.Perimeter();
+
+            Console.WriteLine("square reimplemented perimeter");
+            return 4 * Length;
+        }
+    }
+
+
+    public class Triangle : Figure
+    {
+        public Triangle(int Length) : base(Length)
+        {
+
+
+            Console.WriteLine("triangle created");
+        }
+
+        public override int Perimeter()
+        {
+            var res = base.Perimeter();
+
+            Console.WriteLine("triangle reimplemented perimeter");
+            return 3 * Length;
+        }
+    }
+}
---
+++ QAAutomation/QAAutomation/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Figures;
+
+namespace ConsoleApplication11
+{
+    class Program
+    {
+
+        public delegate int DelegateSample();
+        public delegate void DelegateSample2();
+
+
+        static void Main(string[] args)
+        {
+            var f1 = new Square(3);
+            var f2 = new Triangle(17);
+
+            f1.PerimeterCalculated += F1_PerimeterCalculated;
+
+            var res = f1.Perimeter();
+
+            f1.PerimeterCalculated -= F1_PerimeterCalculated;
+
+            res = f1.Perimeter();
+
+
+
+            DelegateSample del;
+            del = f1.Perimeter;
+
+
+
+            List<Figure> figures = new List<Figure> { f1, f2 };
+
+            foreach (var f in figures)
+                Console.WriteLine(f.Perimeter());
+
+
+        }
+
+        private static void F1_PerimeterCalculated(object sender, string e)
+        {
+            Console.WriteLine("We got the notification!");
+        }
+
+        static void SampleDel2()
+        {
+            Console.WriteLine("Something to output");
+        }
+    }
+}
